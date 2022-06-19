473e43067518ac92a3e906a564df30f1ee0a1289
inadgomezdiaz <cotoop@gmail.com>
2022-06-18 21:41:53 -0300

    Updates

---
+++ precache-manifest.0d2d9fd2de8e498285af4a613f9e2782.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "63c170304e7667d5a480",
+    "url": "/inadgomezdiaz/Click-counter/static/css/main.b9ff00f9.chunk.css"
+  },
+  {
+    "revision": "63c170304e7667d5a480",
+    "url": "/inadgomezdiaz/Click-counter/static/js/main.63c17030.chunk.js"
+  },
+  {
+    "revision": "0cf5f86c9763b075b409",
+    "url": "/inadgomezdiaz/Click-counter/static/js/1.0cf5f86c.chunk.js"
+  },
+  {
+    "revision": "75b4edb21322dbf1336e",
+    "url": "/inadgomezdiaz/Click-counter/static/js/runtime~main.75b4edb2.js"
+  },
+  {
+    "revision": "7a6b9214ee32c63bdd7baf8ffd7bf8c8",
+    "url": "/inadgomezdiaz/Click-counter/static/media/logo-clickcounter.7a6b9214.png"
+  },
+  {
+    "revision": "1015cd8b01f9d3938c828ab554a5be2e",
+    "url": "/inadgomezdiaz/Click-counter/index.html"
+  }
+];
